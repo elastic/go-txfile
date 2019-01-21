71ad94de9546475303aefa99ce0edc6e4347f4f2
urso <steffen.siering@elastic.co>
2019-01-21 10:15:28 +0100

    cleanup magefile

--- magefile.go
+++ magefile.go
@@ -35,10 +35,17 @@ import (
 	"github.com/elastic/go-txfile/dev-tools/lib/mage/mgenv"
 )
 
-type Check mg.Namespace
-type Prepare mg.Namespace
-type Build mg.Namespace
+// Info namespace is used to print additional docs, help messages, and other info.
 type Info mg.Namespace
+
+// Prepare namespace is used to prepare/download/build common depenendencies for other tasks to run.
+type Prepare mg.Namespace
+
+// Check runs pre-build checks on the environment and source code. (e.g. linters)
+type Check mg.Namespace
+
+// Build namespace defines the set of build targets
+type Build mg.Namespace
 
 const buildHome = "build"
 
@@ -53,6 +60,7 @@ var (
 
 // targets
 
+// Env prints the list of defined environment variables
 func (Info) Env() {
 	for _, k := range mgenv.Keys() {
 		v, _ := mgenv.Find(k)
@@ -60,14 +68,23 @@ func (Info) Env() {
 	}
 }
 
+// All runs all Prepare tasks
+func (Prepare) All() { mg.Deps(Prepare.Dirs) }
+
+// Dirs creates requires build directories for storing artifacts
+func (Prepare) Dirs() error { return mkdir("build") }
+
+// Lint runs golint
 func (Check) Lint() error {
 	return errors.New("TODO: implement me")
 }
 
-func (Prepare) Dirs() error {
-	return mkdir("build")
+// Clean removes build artifacts
+func Clean() error {
+	return sh.Rm(buildHome)
 }
 
+// Mage builds the magefile binary for reuse
 func (Build) Mage() error {
 	mg.Deps(Prepare.Dirs)
 
@@ -77,6 +94,7 @@ func (Build) Mage() error {
 	return sh.Run("mage", "-f", "-goos="+goos, "-goarch="+goarch, "-compile", out)
 }
 
+// Test builds the per package unit test executables.
 func (Build) Test() error {
 	mg.Deps(Prepare.Dirs)
 
@@ -93,37 +111,33 @@ func (Build) Test() error {
 	})
 }
 
+// Run package unit tests
 func Test() error {
 	mg.Deps(Prepare.Dirs)
 
 	return withList(gotool.ListProjectPackages, failFastEach, func(pkg string) error {
+		fmt.Println("Test:", pkg)
 		if b, err := gotool.HasTests(pkg); !b {
 			fmt.Printf("Skipping %v: No tests found\n", pkg)
 			return err
 		}
-
-		tst := gotool.Test
-		fmt.Println("Test:", pkg)
 
 		home := path.Join(buildHome, pkg)
 		if err := mkdir(home); err != nil {
 			return err
 		}
 
+		tst := gotool.Test
 		bin := path.Join(home, path.Base(pkg))
 		return tst(
 			tst.Use(useIf(bin, existsFile(bin) && envTestUseBin)),
-			tst.WithCoverage(path.Join(buildHome, pkg, "cover.out")),
+			tst.WithCoverage(path.Join(home, "cover.out")),
 			tst.Short(envTestShort),
 			tst.Out(bin),
 			tst.Package(pkg),
 			tst.Verbose(),
 		)
 	})
-}
-
-func Clean() error {
-	return sh.Rm(buildHome)
 }
 
 // helpers

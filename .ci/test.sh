77cce93c5d7103d9cc589dc4515456331ad4f4b1
Victor Martinez <victormartinezrubio@gmail.com>
2020-12-16 15:56:42 +0000

    [CI] Pipeline (#48)

---
+++ .ci/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# Run the tests
+set +e
+export OUT_FILE="build/test-report.out"
+mkdir -p build
+mage -v test | tee ${OUT_FILE}
+status=$?
+go get -v -u github.com/tebeka/go2xunit
+go2xunit -fail -input ${OUT_FILE} -output "build/junit-${GO_VERSION}.xml"
+
+exit ${status}

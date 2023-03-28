1c10d8071d218134d54879afe16a172721d5f712
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-03-28 15:46:39 +0200

    support test reporting

--- .ci/test.sh
+++ .ci/test.sh
@@ -8,6 +8,6 @@ mkdir -p build
 mage -v test | tee ${OUT_FILE}
 status=$?
 go get -v -u github.com/tebeka/go2xunit
-go2xunit -fail -input ${OUT_FILE} -output "build/junit-${GO_VERSION}.xml"
+go2xunit -fail -input ${OUT_FILE} -output "build/junit-${GO_VERSION}-${RUNNER_OS}.xml"
 
 exit ${status}

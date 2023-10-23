f728a8754a83382aca8de1ba2339a53fe6d77b04
Olga Naydyonock <olga.naidjonoka@elastic.co>
2023-10-23 15:26:29 +0300

    remove .ci folder (#64)

--- .ci/test.bat
+++
@@ -1,5 +0,0 @@
-mkdir -p build
-SET OUT_FILE=build\output-report.out
-mage -v test > %OUT_FILE% | type %OUT_FILE%
-go get -v -u github.com/tebeka/go2xunit
-go2xunit -fail -input %OUT_FILE% -output build\junit-%GO_VERSION%.xml
